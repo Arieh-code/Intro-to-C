af8804d133de63755c3e154a7b654933639040b6
Arieh Norton <arieh.norton@gmail.com>
2021-10-30 21:51:52 +0300

    main done

--- Assignment/Ex0/main.c
+++ Assignment/Ex0/main.c
@@ -2,9 +2,23 @@
 #include"NumClass.h"
 
 void main(){
-    printf("\n You are in main.c \n ");
-    printf("\n Calling hello function");
-    hello();
-    printf("\n calling isArmstrong function\n");
-    isArmstrong();
+    int a, b;
+   printf("\n Hello, how are you? Please enter the two numbers: \n");
+   scanf("%d", &a,b);
+   a = ("%d", a);
+   b = ("%d", b);
+   for(a; a<b; a++){
+     if(isArmstrong(a) == 1){
+         printf(a + " is an armstrong number\n");
+     }  
+     if(isprime(a) == 1){
+         printf(a + " is a prime number\n");
+     }
+     if(isStrong(a) == 1){
+         printf(a + " is a Strong number\n");
+     }
+     if(isPalindrome(a) == 1){
+         printf(a + " is a palindrome number\n" );
+     }
+   }
 }
