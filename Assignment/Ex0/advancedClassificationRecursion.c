91c9fcafd9d1ff674d102661531bb03e4f58c139
Arieh Norton <arieh.norton@gmail.com>
2021-11-01 12:13:35 +0200

    update recursive , done

--- Assignment/Ex0/advancedClassificationRecursion.c
+++ Assignment/Ex0/advancedClassificationRecursion.c
@@ -1,7 +1,3 @@
-/**
- * C program to check palindrome number using recursion
- */
- 
 #include <stdio.h>
 #include <math.h>
 #include "NumClass.h"
@@ -9,8 +5,32 @@
 
 
 
+
+
+// function to calculate the order of the number using recursion
+int order(int num){
+    if(num > 0)
+    return(1 + order(num/10));
+}
+
+
 /**
- * Function to check whether a number is palindrome or not.
+ * Recursive function to find reverse of any number
+ */
+int reverse(int num)
+{
+    /* Find number of digits in num */
+    int digit = order(num)-1;
+    
+    /* Recursion base condition */
+    if(num == 0)
+        return 0;
+
+    return ((num%10 * pow(10, digit)) + reverse(num/10));
+}
+
+/**
+ * Function to check if a number is palindrome or not.
  * This function returns 1 if the number is palindrome otherwise 0.
  */
 int isPalindrome(int num)
@@ -27,51 +47,18 @@ int isPalindrome(int num)
     return 0;
 }
 
-
-/**
- * Recursive function to find reverse of any number
- */
-int reverse(int num)
+// the recursion function that will return a result of armstrong.
+int isArmstronghelper(int num, int order)
 {
-    /* Find number of digits in num */
-    int digit = (int)log10(num);
-    
-    /* Recursion base condition */
-    if(num == 0)
-        return 0;
-
-    return ((num%10 * pow(10, digit)) + reverse(num/10));
+    if(num == 0){
+        return 0; 
+    }
+    return (pow(num%10,order) +isArmstronghelper(num/10, order));
 }
+
 //the main function to check if a numer is armstrong
 int isArmstrong(int num){
-    if(num == isArmstronghelper(num))
+    if(num == isArmstronghelper(num, order(num)))
         return 1; 
     return 0;
-}
-
-// the recursion function that will return a result of armstrong.
-int isArmstronghelper(int num)
-{
-    int len;
-    len = order(num);
-     if(num>0)
-    return (power(num%10,len) +check_ArmstrongNumber(num/10));
-}
-
-// function to calculate the order of the number using recursion
-int order(int num){
-    if(num > 0)
-    return(1 + order(num/10));
-}
-
-
-
-/* Function to calculate x raised to the power y */
-int power(int x, unsigned int y)
-{
-    if (y == 0)
-        return 1;
-    if (y % 2 == 0)
-        return power(x, y / 2) * power(x, y / 2);
-    return x * power(x, y / 2) * power(x, y / 2);
 }
