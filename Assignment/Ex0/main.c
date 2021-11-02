671bdb5b9298428c7efbd243667e5f38a691341b
Arieh Norton <arieh.norton@gmail.com>
2021-11-02 09:09:41 +0200

    added "Done" print

--- Assignment/Ex0/main.c
+++ Assignment/Ex0/main.c
@@ -3,36 +3,38 @@
 
 void main(void){
     int a, b, i;
-   printf("\n Hello, how are you? Please enter the two numbers: \n");
+   printf(" Hello, how are you? Please enter the two numbers: \n");
    scanf("%d %d", &a,&b);
   
   
-   printf("\n isPrime numbers: \n");
+   printf("\n isPrime numbers: ");
    for(i = a; i<=b; i++){
         if(isPrime(i) == 1){
             printf("%d, ", i);         
      } 
    } 
    
-   printf("\n isStrong numbers: \n");
+   printf("\n isStrong numbers: ");
      for(i = a; i<=b; i++){
         if(isStrong(i) == 1){
             printf("%d, ", i);          
      } 
    } 
    
-   printf("\n isPalindrome numbers: \n");
+   printf("\n isPalindrome numbers: ");
      for(i = a; i<=b; i++){
         if(isPalindrome(i) == 1){
             printf("%d, ", i);          
      } 
    } 
    
-   printf("\n isArmostrong numbers: \n");
+   printf("\n isArmostrong numbers: ");
      for(i= a; i<=b; i++){
         if(isArmstrong(i) == 1){
             printf("%d, ", i);          
      } 
    } 
+
+   printf("\n Done! \n");
 
 }
