3d5e1d1b3b79ca8af96151db4583e36013f26186
Arieh Norton <arieh.norton@gmail.com>
2021-11-03 10:50:57 +0200

    fixed is prime

--- Assignment/Ex0/basicClassification.c
+++ Assignment/Ex0/basicClassification.c
@@ -28,8 +28,8 @@ int isStrong(int x){
 
 // is prime function
 int isPrime(int num){
-    if(num<2){
-        return 0;
+    if(num==1){
+        return 1;
     }
     for(int i=2; i<num; i++){
         if(num%i==0){
