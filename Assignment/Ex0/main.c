b001dd6047086967709705be99b76090180babab
Arieh Norton <arieh.norton@gmail.com>
2021-11-03 11:35:05 +0200

    removed comma

--- Assignment/Ex0/main.c
+++ Assignment/Ex0/main.c
@@ -9,28 +9,28 @@ void main(void){
     printf("\n The Armostrong numbers are: ");
      for(i= a; i<=b; i++){
         if(isArmstrong(i) == 1){
-            printf("%d, ", i);          
+            printf("%d ", i);          
      } 
    }
    
     printf("\n The Palindromes are: ");
      for(i = a; i<=b; i++){
         if(isPalindrome(i) == 1){
-            printf("%d, ", i);          
+            printf("%d ", i);          
      } 
    } 
    
    printf("\n The Prime numbers are: ");
    for(i = a; i<=b; i++){
         if(isPrime(i) == 1){
-            printf("%d, ", i);         
+            printf("%d ", i);         
      } 
    } 
 
     printf("\n The Strong numbers are: ");
      for(i = a; i<=b; i++){
         if(isStrong(i) == 1){
-            printf("%d, ", i);          
+            printf("%d ", i);          
      } 
    } 
    
