0de16e36961dcfa670f2e43ad82606709a0c6442
Arieh Norton <arieh.norton@gmail.com>
2021-11-03 10:50:44 +0200

    rearranged printing

--- Assignment/Ex0/main.c
+++ Assignment/Ex0/main.c
@@ -3,38 +3,37 @@
 
 void main(void){
     int a, b, i;
-   printf(" Hello, how are you? Please enter the two numbers: \n");
-   scanf("%d %d", &a,&b);
+    scanf("%d %d", &a,&b);
   
-  
-   printf("\n isPrime numbers: ");
-   for(i = a; i<=b; i++){
-        if(isPrime(i) == 1){
-            printf("%d, ", i);         
-     } 
-   } 
-   
-   printf("\n isStrong numbers: ");
-     for(i = a; i<=b; i++){
-        if(isStrong(i) == 1){
+
+    printf("\n The Armostrong numbers are: ");
+     for(i= a; i<=b; i++){
+        if(isArmstrong(i) == 1){
             printf("%d, ", i);          
      } 
-   } 
+   }
    
-   printf("\n isPalindrome numbers: ");
+    printf("\n The Palindromes are: ");
      for(i = a; i<=b; i++){
         if(isPalindrome(i) == 1){
             printf("%d, ", i);          
      } 
    } 
    
-   printf("\n isArmostrong numbers: ");
-     for(i= a; i<=b; i++){
-        if(isArmstrong(i) == 1){
-            printf("%d, ", i);          
+   printf("\n The Prime numbers are: ");
+   for(i = a; i<=b; i++){
+        if(isPrime(i) == 1){
+            printf("%d, ", i);         
      } 
    } 
 
-   printf("\n Done! \n");
+    printf("\n The Strong numbers are: ");
+     for(i = a; i<=b; i++){
+        if(isStrong(i) == 1){
+            printf("%d, ", i);          
+     } 
+   } 
+   
+   printf("\n");
 
 }
