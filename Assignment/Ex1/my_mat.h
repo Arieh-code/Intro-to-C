f996dc391b0cdca45cfb86d73eff3bbd0c464284
Arieh Norton <arieh.norton@gmail.com>
2021-11-14 23:10:50 +0200

    function i have

---
+++ Assignment/Ex1/my_mat.h
@@ -0,0 +1,10 @@
+#define Max 2
+void make_mat(int mat[Max][Max]);
+
+int exsits(int mat[Max][Max], int a, int b);
+
+void shortest_route(int mat[Max][Max]);
+
+int min(int num1, int num2);
+
+void print_shortestpath(int mat[Max][Max], int a, int b);
