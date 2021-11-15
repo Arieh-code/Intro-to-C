176bdc1eb3b5a945c6b9496828fcfbd141f11a5e
Arieh Norton <arieh.norton@gmail.com>
2021-11-15 09:05:20 +0200

    fixed a problem

--- Assignment/Ex1/main.c
+++ Assignment/Ex1/main.c
@@ -8,27 +8,22 @@ int main(){
     int matrix[Max][Max];
     char input;
     while(input != 'D'){
-    scanf("%c", input);
+    scanf("%c", &input);
     if(input == 'A'){
         make_mat(matrix);
+        shortest_route(matrix);
     }
     else if(input == 'B'){
         int i, j;
         scanf("%d,%d", &i,&j);
-        if(exsits(matrix, i, j) == True){
-            printf("True\n");
-        }
-        else{
-            printf("False\n");
-        }
+        exsits(matrix, i, j);
     }
     else if(input == 'C'){
         int a, b;
         scanf("%d,%d", &a,&b);
-        shortest_route(matrix);
         print_shortestpath(matrix, a, b);
-
     }
     
     }
+    return 0;
 }
