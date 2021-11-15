3e20a6186bc003cc14516682a220c940f27ba059
Arieh Norton <arieh.norton@gmail.com>
2021-11-15 09:06:07 +0200

    updated exisits to void

--- Assignment/Ex1/my_mat.h
+++ Assignment/Ex1/my_mat.h
@@ -1,7 +1,7 @@
 #define Max 10
 void make_mat(int mat[Max][Max]);
 
-int exsits(int mat[Max][Max], int a, int b);
+void exsits(int mat[Max][Max], int a, int b);
 
 void shortest_route(int mat[Max][Max]);
 
