49f8b6052cb076e4c058149903f68e91226fdab6
Arieh Norton <arieh.norton@gmail.com>
2021-12-15 23:29:12 +0200

    header done

---
+++ Assignment/Ex2/ex2.h
@@ -0,0 +1,22 @@
+# define WORD 30
+# define TXT 1024
+
+void makeWord(char word[WORD]);
+
+void makeText(char text[TXT]);
+
+char lowerCase(char c);
+
+int Gimatria(char c);
+
+void Gematria_Sequences(char a[], char b[]);
+
+char convert(char c);
+
+void revstr(char arr[]);
+
+void Atbash_Sequences(char a[], char b[]);
+
+int isAnagram(char* a, char* b); 
+
+void Anagram_Sequences(char a[], char b[]);
