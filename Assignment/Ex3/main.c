1d902cfcb417f488180c81120488140b9e40a273
Arieh Norton <arieh.norton@gmail.com>
2022-01-07 23:48:45 +0200

    done

--- Assignment/Ex3/main.c
+++ Assignment/Ex3/main.c
@@ -13,60 +13,64 @@ int main(void)
     {
         counter++;
         char *tmp = realloc(ptr, (size + 2) * sizeof(*tmp));
-        if(!tmp) {/* handle allocation error */}
+        if(!tmp) {
+            printf("Memory not allocated");
+            exit(0);
+        }
         ptr = tmp;
         ptr[size++] = ch;
         ptr[size] = 0;
     }
-
+    char *ptr_helper = ptr;
     char *ptr_final = NULL;
     size = 0; 
-    while(*ptr){
-        if(*ptr != ' '){
+    while(*ptr_helper){
+        if(*ptr_helper != ' '){
             char *final_temp = realloc(ptr_final, (size+2) * sizeof(*final_temp));
             if(!final_temp){
                 printf("Memory not allocated");
                 exit(0);
             }
             ptr_final = final_temp;
-            ptr_final[size++] = *ptr;
+            ptr_final[size++] = *ptr_helper;
             ptr_final[size] = 0;
-            ptr++;
+            ptr_helper++;
         }
         else{
-            ptr++;
+            ptr_helper++;
         }
     }
-    // free(ptr);
+    free(ptr);
+    char *ptrf_helper = ptr_final;
     pnode temp = NULL;
     pnode *head = &temp;
-    while(*ptr_final){
-        if(*ptr_final == 'A'){
-            ptr_final++;
-            build_graph_cmd(head, ptr_final);
+    while(*ptrf_helper){
+        if(*ptrf_helper == 'A'){
+            ptrf_helper++;
+            build_graph_cmd(head, ptrf_helper);
         }
-        else if(*ptr_final == 'B'){
-            ptr_final++;
-            add_new_node(head, ptr_final);
+        else if(*ptrf_helper == 'B'){
+            ptrf_helper++;
+            add_new_node(head, ptrf_helper);
         }
-        else if(*ptr_final == 'D'){
-            ptr_final++;
-            delete_node_cmd(head, *ptr_final-'0');
+        else if(*ptrf_helper == 'D'){
+            ptrf_helper++;
+            delete_node_cmd(head, *ptrf_helper-'0');
         }
-        else if(*ptr_final == 'S'){
-            ptr_final++;
-            int sp = shortsPath_cmd(temp, ptr_final);
-            printf("Dijsktra shortest path: %d\n", sp);
+        else if(*ptrf_helper == 'S'){
+            ptrf_helper++;
+            int sp = shortsPath_cmd(temp, ptrf_helper);
+            printf("Dijsktra shortest path: %d \n", sp);
         }
-        else if(*ptr_final == 'T'){
-            ptr_final++;
-            TSP_cmd(temp, ptr_final);
+        else if(*ptrf_helper == 'T'){
+            ptrf_helper++;
+            TSP_cmd(temp, ptrf_helper);
         }
         else{
-            ptr_final++;
+            ptrf_helper++;
         }
     }
     deleteGraph_cmd(head);
-    // free(ptr_final);
+    free(ptr_final);
     return 0; 
 }
