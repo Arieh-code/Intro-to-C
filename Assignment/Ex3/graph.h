e30e8ffa71cb05525eb3d6db911372b1c44df061
Arieh Norton <arieh.norton@gmail.com>
2022-01-07 14:47:38 +0200

    updated header

--- Assignment/Ex3/graph.h
+++ Assignment/Ex3/graph.h
@@ -14,6 +14,8 @@ typedef struct GRAPH_NODE_ {
     int node_num;
     pedge edges;
     struct GRAPH_NODE_ *next;
+    int shortest_time;
+    int explored;
 } node, *pnode;
 
 void build_graph_cmd(pnode *head, char *str);
@@ -21,10 +23,12 @@ void insert_node_cmd(pnode *head, int id);
 void delete_node_cmd(pnode *head, int id);
 void printGraph_cmd(pnode head); //for self debug
 void deleteGraph_cmd(pnode* head);
-void shortsPath_cmd(pnode head);
-void TSP_cmd(pnode head);
+int shortsPath_cmd(pnode head,  char *str);
+void TSP_cmd(pnode head, char *str);
 void add_edge_tonode(pnode *head, char *str);
 pnode getNode(pnode *head, int id);
 void add_new_node(pnode *head, char *str);
+int minPath(int* route, int size, pnode head);
+void permute(int *route, int start, int end, int *min_weight, int size, pnode head);
 
 #endif
